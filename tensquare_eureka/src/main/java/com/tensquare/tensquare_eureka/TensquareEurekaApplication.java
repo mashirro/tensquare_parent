fe4bd272f0d065004bf4f8c5c1ede034ab81c8dd
chenzg <18627901759@163.com>
2019-12-26 16:19:16 +0800

    eureka服务注册与发现

---
+++ tensquare_eureka/src/main/java/com/tensquare/tensquare_eureka/TensquareEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.tensquare.tensquare_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class TensquareEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TensquareEurekaApplication.class, args);
+	}
+
+}
---
+++ tensquare_eureka/src/test/java/com/tensquare/tensquare_eureka/TensquareEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tensquare.tensquare_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TensquareEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
